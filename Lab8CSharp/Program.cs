da1ef31a6a8ccd103a7e663ba99d63ad24a4d864
Andriy Fedoryshyn <91829698+AndriyFedoryshyn@users.noreply.github.com>
2024-03-18 00:38:00 +0200

    lab8

--- Lab8CSharp/Program.cs
+++ Lab8CSharp/Program.cs
@@ -1,3 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Lab#8 ");
-//  За бажанням студента косольний проект або WinForm
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        //menu what task to execute using while loop
+        while (true)
+        {
+            Console.WriteLine("Choose task to execute: 1, 2, 3, 4, 5, 6-exit");
+            string task = Console.ReadLine();
+            switch (task)
+            {
+                case "1":
+                    Program1.Task1();
+                    break;  
+                case "2":
+                    Program2.Task2();
+                    break;
+                case "3":
+                    Program3.Task3();
+                    break;
+                case "4":
+                    Program4.Task4();
+                    break;
+                case "5":
+                    Program5.Task5();
+                    break;
+                case "6":
+                    return;
+                default:
+                    Console.WriteLine("Task not found");
+                    break;
+            }
+        }
+    }
+}
